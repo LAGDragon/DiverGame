a601cee37e1fbf906d7c8b2662be13e42ec8f4d7
LAGDragon <dragonlagd@gmail.com>
2017-02-06 14:15:01 -0500

    Add files via upload

---
+++ Game/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+
+	private Rigidbody2D MyRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		MyRigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MyRigidBody.velocity = new Vector2 (moveSpeed, MyRigidBody.velocity.y);
+
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) )
+		{
+			MyRigidBody.velocity = new Vector2 (MyRigidBody.velocity.x, jumpForce);
+		}
+	}
+}
